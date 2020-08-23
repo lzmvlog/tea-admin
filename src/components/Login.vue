60d2534b7df8f1310e5cc9c92c7136663c6db079
Shao Jie <zhang1591313226@163.com>
2020-08-24 00:21:43 +0800

    初始化主页面

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login">
+      登录组件
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+.login{
+  background-color: #2b4b6b;
+}
+</style>
